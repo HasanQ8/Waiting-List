e6ac59edde6ddcabb67a640d3a3e77c5cfc3225f
hasan <hasanfakhra@hotmail.com>
2024-03-16 09:40:54 +0300

    added waitinglist website

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Theme/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/models/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -13,8 +16,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        InterBlack: "InterBlack",
+        InterBold: "InterBold",
+        InterMedium: "InterMedium",
+        InterRegular: "InterRegular",
+      },
     },
   },
+  // corePlugins: {
+  //   aspectRatio: false,
+  // },
   plugins: [],
 };
 export default config;
